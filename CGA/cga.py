846c2751804bfc483b49de430ccf2e2ba8b51c02
inspirelt <inspirelt1994@163.com>
2021-08-02 18:11:29 +0800

    Update cga.py

--- CGA/cga.py
+++ CGA/cga.py
@@ -113,7 +113,7 @@ def cga(xyz,
             intra_features = tf.div_no_nan(tf.reduce_sum(intra_features, axis=-2), tf.reduce_sum(intra_neighbor, axis=-2))
 
             intra_features = tf.concat([intra_features, features], axis=-1)
-            intra_features = conv1d_1x1(intra_features, fdim, 'intra_fc', is_training=is_training, with_bias=False,
+            intra_features = conv1d_1x1(intra_features, 32, 'intra_fc', is_training=is_training, with_bias=False,
                                         init=init,
                                         weight_decay=weight_decay, activation_fn=activation_fn, bn=bn,
                                         bn_momentum=bn_momentum,
@@ -124,7 +124,7 @@ def cga(xyz,
         with tf.variable_scope('drm') as sc:
             concat_features_diff = tf.concat([center_features - neighbor_features, diff_xyz], axis=-1)
             concat_features_diff = tf.reshape(concat_features_diff, [N*n_neighbors, input_dim+3])
-            relation = conv1d_1x1(concat_features_diff, fdim, 'inter_rel', is_training=is_training, with_bias=False, init=init,
+            relation = conv1d_1x1(concat_features_diff, 32, 'inter_rel', is_training=is_training, with_bias=False, init=init,
                               weight_decay=weight_decay, activation_fn=activation_fn, bn=bn, bn_momentum=bn_momentum,
                               bn_eps=bn_eps)
 
@@ -133,7 +133,7 @@ def cga(xyz,
             inter_features = tf.div_no_nan(tf.reduce_sum(inter_features, axis=-2),
                                            tf.reduce_sum(inter_neighbor, axis=-2) + 1e-10)
             inter_features = tf.concat([inter_features, features], axis=-1)
-            inter_features = conv1d_1x1(inter_features, fdim, 'inter_fc', is_training=is_training, with_bias=False,
+            inter_features = conv1d_1x1(inter_features, 32, 'inter_fc', is_training=is_training, with_bias=False,
                                         init=init,
                                         weight_decay=weight_decay, activation_fn=activation_fn, bn=bn,
                                         bn_momentum=bn_momentum,
@@ -143,7 +143,7 @@ def cga(xyz,
         
         with tf.variable_scope('fusion') as sc:
             fused_features = tf.concat([features, intra_features, inter_features], axis=-1)
-            fused_features = conv1d_1x1(fused_features, 2*fdim, 'fusion_feature', is_training=is_training,
+            fused_features = conv1d_1x1(fused_features, fdim, 'fusion_feature', is_training=is_training,
                                             with_bias=False,
                                             init=init,
                                             weight_decay=weight_decay, activation_fn=activation_fn, bn=bn,
